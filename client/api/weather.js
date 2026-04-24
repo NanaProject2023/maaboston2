5f4ca3eaa1934279a74c1b79b06266c4a15c5cb6
NanaProject <nanaproject2023@yahoo.com>
2026-04-23 23:24:16 -0400

    na

--- client/api/weather.js
+++ client/api/weather.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch("https://corsproxy.io/?https://forecast.weather.gov/xml/current_obs/KBOS.rss");
+    const response = await fetch("https://forecast.weather.gov/xml/current_obs/KBOS.rss");
     const data = await response.text();
 
     res.setHeader("Access-Control-Allow-Origin", "*");
